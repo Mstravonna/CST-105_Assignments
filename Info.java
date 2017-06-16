80ba5469efdda13fe0fe471dc4cee4a4d8feb024
Travonna Wilson <travonna@yahoo.com>
2017-06-16 14:32:35 -0500

    My first assigment Week 1 Question 2

---
+++ Info.java
@@ -0,0 +1,14 @@
+
+public class Info {
+
+	public static void main(String[] args) {
+		
+		System.out.println("My name is Travonna Wilson.");
+		System.out.println("My favorite color is Black.");
+		System.out.println("If I won the lottery, I would take care of my family and own a couple of business");
+		
+		
+
+	}
+
+}
