46b678f97d574807ee4a1edac8fc42c5b5cc7b1f
Mstravonna <Travonna@yahoo.com>
2017-07-16 22:36:40 -0500

    Add files via upload

---
+++ NFLPlayer.java
@@ -0,0 +1,12 @@
+//Travonna Wilson
+//7/16/17
+//CST-105
+//this is my own work
+//testing the no-arg constructor
+
+public class NFLPlayer {
+	NFLPlayer nfl_player1 = new NFLPlayer();
+	
+	}
+
+
---
+++ TeamPlayers.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class TeamPlayers {
+
+	public TeamPlayers() {
+		new ArrayList <TeamPlayers>();
+	}
+
+}
